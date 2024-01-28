e2497cae39c931c89d02c334c23c26de9f2cbb8a
hamzahasa067 <hamzahasa067@outlook.com>
2024-01-28 13:16:56 +0200

    added photos-upload

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Photo
 # Register your models here.
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ("name", "image_url")
+    
+admin.site.register(Photo , PhotoAdmin)
--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from cloudinary.uploader import destroy
+from django.db.models.signals import pre_delete
 
 # Create your models here.
+class Photo(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=100)
+    cloudinary_image = CloudinaryField('image' , blank=True) 
+
+    @property
+    def image_url(self):
+        return self.cloudinary_image.url
+
+
+    def __str__(self):
+        return self.name
+    
+def delete_cloudinary_image(sender, instance, **kwargs):
+    image_public_id = instance.cloudinary_image.public_id
+    destroy(image_public_id)
+    
+pre_delete.connect(delete_cloudinary_image, sender=Photo)
